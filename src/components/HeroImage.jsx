cda638bfa254281736912f0ffb480f0f0c76d97e
Pradikshan <pradikshan234@gmail.com>
2024-01-19 20:23:57 +0530

    Implemented hero section

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,9 @@
+export const HeroImage = () => {
+    return (
+        <div className="basis-1/2">
+            <img src="../images/sideways.jpg" alt="hero-image" className="object-cover"/>
+        </div>
+        
+    );
+}
+
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,16 @@
+export const HeroText = () => {
+    return(
+        <div className="basis-1/2 px-3">
+            <h1 class="text-4xl font-extrabold pt-5">
+                Elevate Your Journey in Unrivaled Luxury
+            </h1>
+            <p class="text-lg font-semibold mt-4">
+                Experience the epitome of style and performance with our exclusive fleet of luxury vehicles. Whether it's a sleek sports car, a luxurious sedan, or a commanding SUV, each ride promises an unforgettable journey.
+            </p>
+            <a href="#" className="btn">
+                Explore Our Fleet
+            </a>
+        </div>
+
+    );
+}
