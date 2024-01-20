c47349025f1bbf908a5f87cf47b716465f0629f9
Pradikshan <pradikshan234@gmail.com>
2024-01-20 23:44:08 +0530

    Replaced the image with an icon in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import { IoCarSportOutline } from "react-icons/io5";
+
 const Navbar = () => {
     return (
         <navbar className="">
             <div className="flex flex-row px-1 pb-5 mt-3 border-b-2 border-solid">
-                <div className="flex flex-row basis-1/4">
-                    <img src="../images/cabrio-car.png" alt="logo" className="w-16 px-3"/>
+                <div className="flex flex-row basis-1/4 ps-8">
+                    <IoCarSportOutline style={{fontSize: "30"}}  className="self-center me-2 mt-2"/>
+                    {/* <img src="../images/cabrio-car.png" alt="logo" className="w-16 px-3"/> */}
                     <p className="pt-2 text-2xl font-extrabold"><a href="#">Majestic Wheels</a></p>
                 </div>
                 
