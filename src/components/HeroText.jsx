23f0fd52ae48d6d4e73a07c5d57a4776035d924e
Pradikshan <pradikshan234@gmail.com>
2024-01-20 23:49:06 +0530

    Made adjustments to the font weight

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,10 +1,10 @@
 export const HeroText = () => {
     return(
         <div className="basis-1/2 px-3 ms-3 content-center">
-            <h1 class="text-4xl font-extrabold pt-40">
+            <h1 class="text-4xl font-bold pt-40">
                 Elevate Your Journey in Unrivaled Luxury
             </h1>
-            <p class="text-lg font-semibold mt-4">
+            <p class="text-lg font-medium mt-4">
                 Experience the epitome of style and performance with our exclusive fleet of luxury vehicles. Whether it's a sleek sports car, a luxurious sedan, or a commanding SUV, each ride promises an unforgettable journey.
             </p>
             <button className="btn btn-hero mt-14 w-max px-3 py-3 -ms-1">
