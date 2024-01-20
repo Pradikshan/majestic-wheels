6fa7cd134086e9ba83441fbb5a99ce6d5184937e
Pradikshan <pradikshan234@gmail.com>
2024-01-20 23:44:33 +0530

    Removed icon import

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,6 +1,3 @@
-import { IoCarSportSharp } from "react-icons/io5";
-// import { IoCarSportOutline } from "react-icons/io5";
-// import { FaCar } from "react-icons/fa6";
 
 const InfoCard = ({ 
     cardTitle, 
@@ -10,7 +7,6 @@ const InfoCard = ({
     return (
         <div className="shadow-lg rounded-lg w-96 mx-10 my-16 px-3 py-3">
             <div className="flex flex-col items-center">
-                {/* <IoCarSportSharp style={style} /> */}
                 {cardIcon}
                 <p className="text-3xl">{cardTitle}</p>
                 <p className="text-lg text-center pt-5 italic">{cardText}</p>
