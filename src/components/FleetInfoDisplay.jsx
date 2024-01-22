7bc40f53d63b65265bc745e073830049bf211551
Pradikshan <pradikshan234@gmail.com>
2024-01-22 12:45:32 +0530

    Implemented logic to fetch data from the json file and display it on the FleetInfoDisplay component

--- src/components/FleetInfoDisplay.jsx
+++ src/components/FleetInfoDisplay.jsx
@@ -17,71 +17,89 @@ const FleetInfoDisplay = () => {
         .catch(setError);
     }, []);
 
-    console.log(data);
+    // console.log(data);
+
+    if (loading) return <h1>Loading...</h1>;
+
+    if (error)
+        return <p>{JSON.stringify(error)}</p>;
+
+    if(!data) return null;
+
+
 
     
 
     return (
         <div className="grid grid-cols-3 gap-4 mx-8">
-            <div className="col-start-1 col-end-2">
+            {data.map((category) => (
+            <>
+            <div key={category.categoryId} className="col-start-1 col-end-2">
                 <div className="flex flex-row">
-                    <p>Vehicle category</p>
-                    <p>Vehicle category</p>
+                    <p>{category.category}</p>
                     {/* <img src="../images/sideways.jpg" alt="vehicle-image"/> */}
                 </div>
             </div>
 
             <div className="col-start-1 col-end-1 row-start-2">
                 <div className="flex flex-col">
-                    {/* <p>HEllo</p> */}
-                    <button>Vehicle name</button>
-                    <button>Vehicle name</button>
-                    <button>Vehicle name</button>
+                    {category.vehicles.map((vehicle) => (
+                        <button key={vehicle.id}>{vehicle.model}</button>
+                    ))}
                 </div>
             </div>
 
             <div className="col-start-2 col-end-2 row-start-2">
-                <img src="../images/sideways.jpg" alt="vehicle-image"/>
+                {category.vehicles.map((vehicle) => (
+                    <img key={vehicle.id} src={vehicle.img} alt="vehicle"/>
+                ))}
             </div>
 
             <div className="col-start-3 col-end-3 row-start-2">
-                <table class="table-auto"> 
-                    <tr>
-                        <th>Manufacturer</th>
-                        <td>Audi</td>
-                    </tr>
-                    
-                    <tr>
-                        <th>Model</th>
-                        <td>A7</td>
-                    </tr>
+                {category.vehicles.map((vehicle) =>
+                    <table class="table-auto"> 
+                        <tbody>
+                            <tr>
+                                <th>Manufacturer</th>
+                                <td>{vehicle.manufacturer}</td>
+                            </tr>
+                            
+                            <tr>
+                                <th>Model</th>
+                                <td>{vehicle.model}</td>
+                            </tr>
 
-                    <tr>
-                        <th>Engine size</th>
-                        <td>2000cc</td>
-                    </tr>
+                            <tr>
+                                <th>Engine size</th>
+                                <td>{vehicle.engine_size}</td>
+                            </tr>
 
-                    <tr>
-                        <th>Year</th>
-                        <td>2019</td>
-                    </tr>
+                            <tr>
+                                <th>Year</th>
+                                <td>{vehicle.year}</td>
+                            </tr>
 
-                    <tr>
-                        <th>Doors</th>
-                        <td>4</td>
-                    </tr>
+                            <tr>
+                                <th>Doors</th>
+                                <td>{vehicle.doors}</td>
+                            </tr>
 
-                    <tr>
-                        <th>Transmission</th>
-                        <td>Automatic</td>
-                    </tr>
+                            <tr>
+                                <th>Transmission</th>
+                                <td>{vehicle.transmission}</td>
+                            </tr>
 
-                    <tr>
-                        <th>Fuel</th>
-                        <td>Petrol</td>
-                    </tr>
-                </table>
-            </div>
+                            <tr>
+                                <th>Fuel</th>
+                                <td>{vehicle.fuel}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                )}
+            </div>  
+            </>
+            ))}
+            
         </div>
 
     );
