d7a4fff6ce1a5d1513d81ffc9a1bd167fd7424fc
Pradikshan <pradikshan234@gmail.com>
2024-01-23 21:47:43 +0530

    Changed font size and font weight

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -1,7 +1,28 @@
-const AboutUsSection = () => {
+import InfoCard from "./InfoCard";
+import { PiBuildingsThin } from "react-icons/pi";
 
-    
 
+export const AboutUsSection = () => {
+    const style = {fontSize: "65"};
+
+    return(
+        <div className="mt-32">
+            <h1 className="text-center text-5xl font-extrabold">Why choose us?</h1>
+            <div className="flex flex-row justify-center">
+                <InfoCard 
+                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardTitle={"Unparalleled Expertise"} 
+                cardText={"Establishing excellence for over two decades, we've been at the forefront of providing exceptional services, setting the standard in the industry"}/>
+                <InfoCard 
+                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardTitle={"Safety First, Always"} 
+                cardText={"Our unwavering commitment to safety ensures your peace of mind throughout every moment of your travels"}/>
+                <InfoCard 
+                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardTitle={"Green Travel Advocate"} 
+                cardText={"Leading the way in sustainability, we're dedicated to eco-friendly practices, making your journey not only enjoyable but also environmentally conscious"}/>
+            </div>
+        </div>
+
+    );
 }
-
-export default AboutUsSection;
--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -8,20 +8,21 @@ export const InfoSection = () => {
 
     return(
         <div className="mt-32">
-            <h1 className="text-center text-5xl font-medium">Why choose us?</h1>
+            <h1 className="text-center text-5xl font-extrabold">Effortlessly arrange your ride</h1>
             <div className="flex flex-row justify-center">
-                <InfoCard 
-                cardIcon={<PiCarProfileThin  style={style}/>} 
-                cardTitle={"Arrive in Style"} 
-                cardText={"Experience unmatched luxury as you make a grand entrance in our fleet of stylish and meticulously maintained vehicles."}/>
                 <InfoCard 
                 cardIcon={<PiCallBellThin style={style}/>} 
                 cardTitle={"Exceptional Service"} 
-                cardText={"Our commitment to excellence extends beyond our vehicles. Enjoy personalized and attentive service that caters to your every need."}/>
+                cardText={"Our commitment to excellence extends beyond our vehicles. Enjoy personalized and attentive service that caters to your every need"}/>
                 <InfoCard 
                 cardIcon={<PiContactlessPaymentThin style={style}/>}
                 cardTitle={"Semaless Experience"} 
-                cardText={"From reservation to drop-off, we ensure a seamless and stress-free experience, allowing you to focus on the moments that matter."}/>
+                cardText={"From reservation to drop-off, we ensure a seamless and stress-free experience, allowing you to focus on the moments that matter"}/>
+                <InfoCard 
+                cardIcon={<PiCarProfileThin  style={style}/>} 
+                cardTitle={"Arrive in Style"} 
+                cardText={"Experience unmatched luxury as you make a grand entrance in our fleet of stylish and meticulously maintained vehicles"}/>
+
                 
             </div>
         </div>
