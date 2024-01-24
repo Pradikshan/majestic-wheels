f17657e8cbae831a58b70fa2487bed9558bbf78d
Pradikshan <pradikshan234@gmail.com>
2024-01-24 19:44:09 +0530

    Changed <h1> tag to <p> tag

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,9 +1,9 @@
 export const HeroText = () => {
     return(
         <div className="basis-1/2 px-3 ms-3 content-center">
-            <h1 class="text-4xl font-bold pt-40">
+            <p class="text-4xl font-bold pt-40">
                 Elevate Your Journey in Unrivaled Luxury
-            </h1>
+            </p>
             <p class="text-lg font-medium mt-4">
                 Experience the epitome of style and performance with our exclusive fleet of luxury vehicles. Whether it's a sleek sports car, a luxurious sedan, or a commanding SUV, each ride promises an unforgettable journey.
             </p>
