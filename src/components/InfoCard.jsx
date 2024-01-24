9acc6b89b951f22eab338b7741cfe0b5a5d8dde4
Pradikshan <pradikshan234@gmail.com>
2024-01-24 19:43:48 +0530

    Changed cardTitle font weight

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -8,7 +8,7 @@ const InfoCard = ({
         <div className="shadow-lg rounded-lg w-1/4 mx-10 my-16 px-3 py-3 transition ease-in-out delay-100 hover:scale-110">
             <div className="flex flex-col items-center">
                 {cardIcon}
-                <p className="text-2xl cursor-default">{cardTitle}</p>
+                <p className="text-2xl cursor-default font-semibold">{cardTitle}</p>
                 <p className="text-md text-center pt-5 pb-3 cursor-default">{cardText}</p>
             </div>
         </div>
