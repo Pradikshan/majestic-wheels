17c2fbc481d4ca7aab7654c99df4b02f88481550
Pradikshan <pradikshan234@gmail.com>
2024-01-24 15:31:59 +0530

    Implemeneted TestimonialSection component

---
+++ src/components/TestimonialSection.jsx
@@ -0,0 +1,15 @@
+import TestimonialCard from "./TestimonialCard";
+
+
+export const TestimonialSection = () => {
+    
+    return(
+        <div className="mt-32">
+            <h1 className="text-center text-5xl font-extrabold">Why choose us?</h1>
+            <div className="flex flex-row justify-center">
+                
+            </div>
+        </div>
+
+    );
+}
