e0e616586b5130c6531334979e6c6c3a26b7c60b
Pradikshan <pradikshan234@gmail.com>
2024-01-24 17:10:05 +0530

    Changed layout from flex to grid

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -5,8 +5,25 @@ const TestimonialCard = ({ image, name, testimonial }) => {
     // const style = { color: "text-yellow-300"}
     
     return (
-        <div className="rounded-lg shadow-lg border-2 border-black w-max">
-            <div className="flex flex-col">
+        <div className="grid grid-cols-2 rounded-lg shadow-lg border-2 border-black w-1/4">
+            <div className="col-start-1 col-end-1 row-start-1">
+                <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
+            </div>
+
+            <div className="col-start-2 col-end-2 row-start-1">
+                <div className="flex flex-col">
+                    <p>{name}</p>
+                    <div className="flex flex-row">
+                        <CiStar className="text-yellow-300" />
+                        <CiStar className="text-yellow-300" />
+                        <CiStar className="text-yellow-300" />
+                        <CiStar className="text-yellow-300" />
+                        <CiStar className="text-yellow-300" />
+                    </div>
+                    <p>{testimonial}</p>
+                </div>
+            </div>
+            {/* <div className="flex flex-col">
                 <div className="flex flex-row overflow-hidden">
                     <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
                     <div className="flex flex-col">
@@ -23,7 +40,7 @@ const TestimonialCard = ({ image, name, testimonial }) => {
 
                 </div>
                 <p>{testimonial}</p>
-            </div>
+            </div> */}
         </div>
 
 
