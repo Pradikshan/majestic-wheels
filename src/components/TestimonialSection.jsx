6abd5d991b2420eb6293aeda3c9b91cad52f09f0
Pradikshan <pradikshan234@gmail.com>
2024-01-24 16:53:46 +0530

    Added headings and implemented card layout

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -4,12 +4,18 @@ import TestimonialCard from "./TestimonialCard";
 export const TestimonialSection = () => {
     
     return(
-        <div className="mt-32">
-            <h1 className="text-center text-5xl font-extrabold">Why choose us?</h1>
-            <div className="flex flex-row justify-center">
-                
+        <>
+            <div className="mt-32 flex flex-col items-center justify-center">
+                <h1 className="text-center text-5xl font-extrabold">Customer testimonials</h1>
+                <p className="text-lg">Discover what our satisfied customers have to say about their extraordinary experiences with us</p>
             </div>
-        </div>
+
+            <div className="flex flex-row justify-center">
+                <TestimonialCard image={"../images/portrait.jpg"} name={"Mary Ann"} testimonial={"Realy long tesimonial for testing purposes "} />
+                <TestimonialCard image={"../images/portrait.jpg"} name={"Mary Ann"} testimonial={"Realy long tesimonial for testing purposes gadhshjajhkdajhsdhjahjsahjahasjkdhajsdhj jhadashdhsja"} />
+                <TestimonialCard image={"../images/portrait.jpg"} name={"Mary Ann"} testimonial={"Realy long tesimonial for testing purposes gadhshjajhkdajhsdhjahjsahjahasjkdhajsdhj jhadashdhsja"} />
+            </div>
+        </>
 
     );
 }
