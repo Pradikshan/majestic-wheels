82ffae915c314f88cd4cfae85b091c7560101943
Pradikshan <pradikshan234@gmail.com>
2024-01-24 19:42:41 +0530

    Changed icons

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -1,5 +1,7 @@
 import InfoCard from "./InfoCard";
-import { PiBuildingsThin } from "react-icons/pi";
+import { AiOutlineSafetyCertificate } from "react-icons/ai";
+import { PiBuildings } from "react-icons/pi";
+import { LuLeaf } from "react-icons/lu";
 
 
 export const AboutUsSection = () => {
@@ -10,15 +12,15 @@ export const AboutUsSection = () => {
             <h1 className="text-center text-5xl font-extrabold">Why choose us?</h1>
             <div className="flex flex-row justify-center">
                 <InfoCard 
-                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardIcon={<PiBuildings  style={style}/>} 
                 cardTitle={"Unparalleled Expertise"} 
                 cardText={"Establishing excellence for over two decades, we've been at the forefront of providing exceptional services, setting the standard in the industry"}/>
                 <InfoCard 
-                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardIcon={<AiOutlineSafetyCertificate  style={style}/>} 
                 cardTitle={"Safety First, Always"} 
                 cardText={"Our unwavering commitment to safety ensures your peace of mind throughout every moment of your travels"}/>
                 <InfoCard 
-                cardIcon={<PiBuildingsThin  style={style}/>} 
+                cardIcon={<LuLeaf  style={style}/>} 
                 cardTitle={"Green Travel Advocate"} 
                 cardText={"Leading the way in sustainability, we're dedicated to eco-friendly practices, making your journey not only enjoyable but also environmentally conscious"}/>
             </div>
--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,7 +1,7 @@
 import InfoCard from "./InfoCard";
-import { PiContactlessPaymentThin } from "react-icons/pi";
-import { PiCarProfileThin } from "react-icons/pi";
-import { PiCallBellThin } from "react-icons/pi";
+import { PiContactlessPayment } from "react-icons/pi";
+import { PiCarProfile } from "react-icons/pi";
+import { PiCallBell } from "react-icons/pi";
 
 export const InfoSection = () => {
     const style = {fontSize: "65"};
@@ -11,15 +11,15 @@ export const InfoSection = () => {
             <h1 className="text-center text-5xl font-extrabold">Effortlessly arrange your ride</h1>
             <div className="flex flex-row justify-center">
                 <InfoCard 
-                cardIcon={<PiCallBellThin style={style}/>} 
+                cardIcon={<PiCallBell style={style}/>} 
                 cardTitle={"Exceptional Service"} 
                 cardText={"Our commitment to excellence extends beyond our vehicles. Enjoy personalized and attentive service that caters to your every need"}/>
                 <InfoCard 
-                cardIcon={<PiContactlessPaymentThin style={style}/>}
+                cardIcon={<PiContactlessPayment style={style}/>}
                 cardTitle={"Semaless Experience"} 
                 cardText={"From reservation to drop-off, we ensure a seamless and stress-free experience, allowing you to focus on the moments that matter"}/>
                 <InfoCard 
-                cardIcon={<PiCarProfileThin  style={style}/>} 
+                cardIcon={<PiCarProfile  style={style}/>} 
                 cardTitle={"Arrive in Style"} 
                 cardText={"Experience unmatched luxury as you make a grand entrance in our fleet of stylish and meticulously maintained vehicles"}/>
 
