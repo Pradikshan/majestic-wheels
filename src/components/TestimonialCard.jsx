62abbad4808d6ffe8391768385ebc9145147ba37
Pradikshan <pradikshan234@gmail.com>
2024-01-24 19:43:15 +0530

    Changed layout from grid to flex

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,12 +1,12 @@
-import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa6";
 
 const TestimonialCard = ({ image, name, testimonial }) => {
 
     // const style = { color: "text-yellow-300"}
     
     return (
-        <div className="grid grid-cols-2 rounded-lg shadow-lg border-2 border-black w-1/4">
-            <div className="col-start-1 col-end-1 row-start-1">
+        <div className="flex flex-row rounded-lg shadow-lg w-3/4 px-2 py-2 m-5">
+            {/* <div className="col-start-1 col-end-1 row-start-1">
                 <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
             </div>
 
@@ -22,25 +22,21 @@ const TestimonialCard = ({ image, name, testimonial }) => {
                     </div>
                     <p>{testimonial}</p>
                 </div>
-            </div>
-            {/* <div className="flex flex-col">
-                <div className="flex flex-row overflow-hidden">
-                    <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
-                    <div className="flex flex-col">
-                        <p>{name}</p>
-                        <div className="flex flex-row">
-                            <CiStar className="text-yellow-300" />
-                            <CiStar className="text-yellow-300" />
-                            <CiStar className="text-yellow-300" />
-                            <CiStar className="text-yellow-300" />
-                            <CiStar className="text-yellow-300" />
-                        </div>
-
-                    </div>
-
-                </div>
-                <p>{testimonial}</p>
             </div> */}
+            <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
+
+            <div className="flex flex-col ps-3 text-wrap">
+                <p className="font-bold text-lg">{name}</p>
+                <div className="flex flex-row">
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                    <FaStar className="star" />
+                </div>
+                {/* <p className="font-md ">{`"${testimonial}"`}</p> */}
+                <p className="text-base mt-3">{testimonial}</p>
+            </div>
         </div>
 
 
