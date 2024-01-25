4daabddcaa6d05b6c9fb56732b75e005a3a31bbf
Pradikshan <pradikshan234@gmail.com>
2024-01-25 13:05:52 +0530

    Added logic for fetching FAQ data

--- src/components/FAQSection.jsx
+++ src/components/FAQSection.jsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
 import DropdownCard from "./DropdownCard";
 
 const FAQSection = () => {
+
+    const [data, setData] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('../faq.json')
+        .then((response) => response.json())
+        .then(setData)
+        .then(() => setLoading(false))
+        .catch(setError)
+    }, []);
+
+    if (loading) return <h1>Loading...</h1>;
+
+    if (error)
+        return <p>{JSON.stringify(error)}</p>;
+
+    if(!data) return null;
+
+
+    // console.log(data);
+
+    return (
+    
+    );
 
 }
 
