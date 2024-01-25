bd509a889deaf1e488ac85804ed98843f0a8720f
Pradikshan <pradikshan234@gmail.com>
2024-01-25 13:07:14 +0530

    Implemented logic to display dropdown content when clicked

--- src/components/DropdownCard.jsx
+++ src/components/DropdownCard.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 
+import { FaMinus } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa6";
 
-const DropdownCard = () => {
+const DropdownCard = ({ title, dropdown}) => {
+
+    const [active, setActive] = useState(false);
+
+    const handleTitleClick = () => {
+        setActive(!active);
+    }
+
+    return (
+        <div className={`border-t-2 border-black w-3/4 py-5 transition-all duration-150 ease-in-out overflow-hidden ${active ? 'text-white bg-blue-950' : ''}`} onClick={handleTitleClick}>
+            <div className="flex flex-row justify-between px-3 cursor-pointer text-lg font-semibold">
+                <p>{title}</p>
+                {active ? <FaMinus /> : <FaPlus />}
+            </div>
+            {active ? 
+            <p className={`px-3 pt-3 ${active ? 'h-auto' : 'overflow-hidden'} transition-all duration-150 ease-in-out`}>
+                {dropdown}
+            </p>
+            : <></>
+        }
+        </div>
+
+    );
 
 }
 
