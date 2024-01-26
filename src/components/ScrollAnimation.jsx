059a83315b348879098704c8e4bd5943aaa395f2
Pradikshan <pradikshan234@gmail.com>
2024-01-26 19:42:24 +0530

    Added logic to display components on scroll

--- src/components/ScrollAnimation.jsx
+++ src/components/ScrollAnimation.jsx
@@ -3,7 +3,31 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 const ScrollAnimation = ({ children }) => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
 
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={{
+        visible: { opacity: 1 },
+        hidden: { opacity: 0 },
+      }}
+      transition={{ duration: 1}}
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default ScrollAnimation;
