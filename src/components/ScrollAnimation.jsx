e20be09665ce1db520a571fdec0c3ba41befcf72
Pradikshan <pradikshan234@gmail.com>
2024-01-26 19:42:03 +0530

    Implemented ScrollAnimation component

---
+++ src/components/ScrollAnimation.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const ScrollAnimation = ({ children }) => {
+
+};
+
+export default ScrollAnimation;
