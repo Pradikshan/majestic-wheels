2c41317d7b1934610282e61f6d4d046dda9f83e2
Pradikshan <pradikshan234@gmail.com>
2024-01-27 18:44:43 +0530

    Made the layout responsive

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -3,13 +3,13 @@ import { Link, animateScroll as scroll } from 'react-scroll';
 export const HeroText = () => {
     return(
         <div className="basis-1/2 px-3 ms-3 content-center">
-            <p class="text-4xl font-bold pt-40">
+            <p class="text-4xl font-bold lg:pt-40 sm:pt-20">
                 Elevate Your Journey in Unrivaled Luxury
             </p>
             <p class="text-lg font-medium mt-4">
                 Experience the epitome of style and performance with our exclusive fleet of luxury vehicles. Whether it's a sleek sports car, a luxurious sedan, or a commanding SUV, each ride promises an unforgettable journey.
             </p>
-            <button className="btn btn-hero mt-14 w-max px-3 py-3 -ms-1">
+            <button className="btn btn-hero lg:mt-14 sm:my-8 w-max px-3 py-3 -ms-1">
                 <Link to="vehicles" smooth={true} duration={500}>
                     Explore Our Fleet
                 </Link>
