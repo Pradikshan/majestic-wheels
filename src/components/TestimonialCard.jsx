02c579167c73f728cfcc7d333a9d185a598324a0
Pradikshan <pradikshan234@gmail.com>
2024-01-28 15:05:03 +0530

    Made the component responsive

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -5,7 +5,7 @@ const TestimonialCard = ({ image, name, testimonial }) => {
     // const style = { color: "text-yellow-300"}
     
     return (
-        <div className="flex flex-row rounded-lg shadow-lg w-3/4 p-5 m-5 bg-slate-50">
+        <div className="flex flex-col sm:flex-row rounded-lg shadow-lg w-3/4 p-5 m-5 bg-slate-50">
             <img src={`${image}`} alt="customer" className="w-24 h-24 object-cover rounded-full "/>
 
             <div className="flex flex-col ps-3 text-wrap">
@@ -17,8 +17,13 @@ const TestimonialCard = ({ image, name, testimonial }) => {
                     <FaStar className="star" />
                     <FaStar className="star" />
                 </div>
+                {window.innerWidth >= 600 ? (
                 <p className="text-base mt-3">{testimonial}</p>
+                ) : null}
             </div>
+            {window.innerWidth <= 600 ? (
+            <p className="text-base mt-3 sm:mt-0 sm:ml-3">{testimonial}</p>
+            ) : null}
         </div>
 
 
